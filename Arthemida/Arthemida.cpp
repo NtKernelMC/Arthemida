3accd7cd0e577e7aa94b742c782dccc13a3da717
hadeweek@gmail.com <Kernel>
2020-06-30 17:41:30 +0300

    IAT Fix #1

--- Arthemida/Arthemida.cpp
+++ Arthemida/Arthemida.cpp
@@ -241,23 +241,6 @@ void __stdcall ART_LIB::ArtemisLibrary::MemoryScanner(ArtemisConfig* cfg)
 	if (cfg == nullptr) return;
 	while (true)
 	{
-		auto FindSequence = [](DWORD_PTR z) -> bool
-		{
-			bool complete_sequence = false;
-			__try
-			{
-				for (DWORD x = 0; x < (10 * 6); x += 0x6)
-				{
-					if (*(BYTE*)(z + x) == 0xFF && *(BYTE*)(x + z + 0x1) == 0x25)
-					{
-						complete_sequence = true;
-					}
-					else complete_sequence = false;
-				}
-			}
-			__except (EXCEPTION_EXECUTE_HANDLER) {};
-			return complete_sequence;
-		};
 		auto WatchMemoryAllocations = [&, cfg]
 		(const void* ptr, size_t length, MEMORY_BASIC_INFORMATION* info, int size)
 		{
@@ -269,24 +252,36 @@ void __stdcall ART_LIB::ArtemisLibrary::MemoryScanner(ArtemisConfig* cfg)
 				MEMORY_BASIC_INFORMATION* i = &info[0];
 				if ((i->State != MEM_FREE && i->State != MEM_RELEASE) && i->Type & (MEM_IMAGE | MEM_PRIVATE) && i->Protect & mask)
 				{
-					bool complete_sequence = false;
+					bool complete_sequence = false; DWORD_PTR foundIAT = 0x0;
 					if (i->RegionSize > 0x1000 && i->RegionSize != 0x7D000 && i->RegionSize != 0xF000)
 					{
 						for (DWORD_PTR z = (DWORD_PTR)ptr; z < ((DWORD_PTR)ptr + i->RegionSize); z++)
 						{
-							complete_sequence = FindSequence(z);
+							for (DWORD x = 0; x < (10 * 6); x += 0x6)
+							{
+								if ((x + z) < ((DWORD_PTR)ptr + i->RegionSize) && (x + z + 0x1) < ((DWORD_PTR)ptr + i->RegionSize))
+								{
+									if (*(BYTE*)(z + x) == 0xFF && *(BYTE*)(x + z + 0x1) == 0x25)
+									{
+										foundIAT = (x + z);
+										complete_sequence = true;
+									}
+									else complete_sequence = false;
+								}
+								else complete_sequence = false;
+							}
 							if (complete_sequence)
 							{
-								if (!IsMemoryInModuledRange(i->BaseAddress))
+								if (!IsMemoryInModuledRange((PVOID)z))
 								{
 									typedef DWORD(__stdcall* LPFN_GetMappedFileNameA)(HANDLE hProcess, LPVOID lpv, LPCSTR lpFilename, DWORD nSize);
 									LPFN_GetMappedFileNameA g_GetMappedFileNameA = nullptr; HMODULE hPsapi = LoadLibraryA("psapi.dll");
 									g_GetMappedFileNameA = (LPFN_GetMappedFileNameA)GetProcAddress(hPsapi, "GetMappedFileNameA");
 									char MappedName[256]; memset(MappedName, 0, sizeof(MappedName));
-									g_GetMappedFileNameA(GetCurrentProcess(), i->BaseAddress, MappedName, sizeof(MappedName));
+									g_GetMappedFileNameA(GetCurrentProcess(), (PVOID)z, MappedName, sizeof(MappedName));
 									if (strlen(MappedName) < 4 && !IsVecContain(cfg->ExcludedImages, i->BaseAddress))
 									{
-										ARTEMIS_DATA data; data.baseAddr = i->BaseAddress;
+										ARTEMIS_DATA data; data.baseAddr = (PVOID)foundIAT;
 										data.MemoryRights = i->Protect; data.regionSize = i->RegionSize;
 										data.dllName = "unknown"; data.dllPath = "unknown";
 										data.type = DetectionType::ART_MANUAL_MAP;
