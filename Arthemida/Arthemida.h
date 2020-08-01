3c646f73720105bb4b23ecac222e54e6f7c6d78b
holmes0 <monsterlight1551@gmail.com>
2020-08-01 15:19:05 +0400

    Fix g_cfg (extern)

--- Arthemida/Arthemida.h
+++ Arthemida/Arthemida.h
@@ -68,4 +68,4 @@ namespace ART_LIB
 		static void __stdcall CheckLauncher(ArtemisConfig* cfg);
 	};
 };
-static ART_LIB::ArtemisLibrary::ArtemisConfig* g_cfg;
+extern ART_LIB::ArtemisLibrary::ArtemisConfig* g_cfg;
